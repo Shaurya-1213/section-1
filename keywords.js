a23d9546c3fdbdf46501ab526463c39d19f7221c
Shaurya-1213 <shauryasingh2033@gmail.com>
2024-03-29 16:53:12 -0400

    JavaScript

---
+++ keywords.js
@@ -0,0 +1,13 @@
+n = 346;
+
+if(n%2==0){
+  console.log('even');
+  var num= 'hello';
+}
+else{
+  console.log('odd');
+}
+
+const pi= 3.14732;
+console.log(num);
+console.log(pi);
---
+++ loops.js
@@ -0,0 +1,22 @@
+for (let i = 100;i<=350;i++){
+  if(i%7==0 || i%11==0){
+  console.log(i);
+}
+}
+
+const fruits = ['apple' , 'mango' , 'guava' , 'grapes']
+for( let f of fruits){
+  console.log(f);
+}
+console.log(fruits);
+
+let a=10;
+while(a<20){
+  console.log(a);
+  a+=2;
+}
+
+let b = 10;
+do{
+  console.log(b);
+} while (b > 20)
